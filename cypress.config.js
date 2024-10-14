3371b518c7a9a4c0866b6291e37aaf72d45dcbcb
Ali Ahmad <apple@ali-MPB.local>
2024-10-14 14:02:00 +0500

    added height and width and test 0 and 2

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
     excludeSpecPattern: 'cypress/e2e/examples/**/*',
-    baseUrl: 'https://practicesoftwaretesting.com/',
+    baseUrl: 'https://practicesoftwaretesting.com',
     env: {
       hideXhr: true,
     },
--- cypress/e2e/1-test.cy.js
+++ cypress/e2e/1-test.cy.js
@@ -1,4 +1,16 @@
 describe(`Cypress: Do's and Don't`, () => {
+  it('Set BaseUrl', () => {
+    //.  BaseUrl: Set baseurl in the cypress.config.js file and use it in your tests by using / or nothing (keeping it empty) in your cy.visit()
+    // cy.visit('https://practicesoftwaretesting.com/') // <--- Don't
+    // cy.visit('') <--- Do
+    // cy.visit('/') // <--- Do
+    
+    // Let's say if you want to visit an endpoint, how would you do this keeping the baseurl in the cypress.config.js file?
+    
+    // cy.visit('https://practicesoftwaretesting.com/contact') // <--- Don't
+    // cy.visit('contact') // <--- Do
+    cy.visit('/contact') // <--- Do
+  })
   it('1: Cypress Asynchronous Behaviour', () => {
     //. Note: Cypress commands are asynchronous and non-blocking by default. Cypress commands like cy.visit() and cy.request() are added to the command queue
     //. and executed asynchronously. Meanwhile, the synchronous expect() statement is executed immediately, without waiting for the preceding commands to finish.
@@ -6,4 +18,9 @@ describe(`Cypress: Do's and Don't`, () => {
     cy.request('/').its('status').should('eq', 200)
     expect(true).to.be.true
   })
+  
+  it('2: Implicit vs Explicit Wait', () => {
+    //cy.visit('/',{timeout:100})
+  }
+)
 })
